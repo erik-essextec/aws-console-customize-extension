56a813bcf8e4dc52e8d4abc4de1d22222a88c418
Erik Sorensen <47573278+erik-essextec@users.noreply.github.com>
2021-12-15 21:25:16 -0500

    typo

--- extension.js
+++ extension.js
@@ -16,7 +16,6 @@ $(function() {
     // probably not ideal, but it works
     var acctElm = $("#menu--account > div.globalNav-0335 > div > div:nth-child(1) > span:nth-child(2)");
     var acct = acctElm.text().replaceAll('-', '');
-    var acct = acctElm.text();
     
     var regions = location.search.match(/region=(.*?)(&|$)/);
     var region = "";
